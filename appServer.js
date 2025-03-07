a400149d1acd61731e10358bc2fe4f1ca2d1e0c9
Enow-brenda <enowbrenda91106@gmail.com>
2025-03-07 07:11:00 +0100

    added the routes for the user pages

--- appServer.js
+++ appServer.js
@@ -7,9 +7,37 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Sample Route
+
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+// Set up routes to serve HTML files
 app.get('/', (req, res) => {
-    res.send('Financial App API is Running');
+    res.sendFile(path.join(__dirname, 'frontend/landingPage.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/login.html'));
+});
+app.get('/signup', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/signup.html'));
+});
+app.get('/user/dashboard', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/user/dashboard.html'));
+});
+app.get('/user/myexpenses', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/user/expenses.html'));
+});
+app.get('/user/education', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/user/lessons.html'));
+});
+app.get('/user/myprofile', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/user/profile.html'));
+});
+app.get('/user/savings', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/user/saving.html'));
+});
+app.get('/user/debt_control', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/user/debtManagement.html'));
 });
 
 const PORT = process.env.PORT || 5000;
